1cfec2b72544117e405fb27103201dc39ff33abb
Evgeniy <kuzntsovzheka@gmail.com>
2025-11-24 00:04:36 +0400

    httpserver

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{"message": "pong"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
 
 func main() {
-	fmt.Println("Welcome new project!")
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/ping", pingHandler)
+
+	fmt.Println("Сервер запущен на http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
